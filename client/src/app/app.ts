64368d3a0b9db522881e3d34013d8717a438bb0e
Kevin <jimenez.kevin0601@gmail.com>
2025-11-22 22:55:26 -0600

    Add https secure certificate

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,4 +1,6 @@
-import { Component} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal} from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +8,21 @@ import { Component} from '@angular/core';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected title = 'client';
+export class App implements OnInit{
+  private http = inject(HttpClient); //This inject method is used instead of contructor
+  protected title = 'Dating App';
+  protected members = signal<any>([]);
+
+  async ngOnInit() { //Since we implemented an interface OnInit we need to implement this method
+    this.members.set(await this.getMembers())
+  }
+
+  async getMembers(){
+    try{
+      return lastValueFrom(this.http.get("https://localhost:5001/api/members"));
+    }catch (error){
+      console.log(error);
+      throw error;
+    }
+  }
 }
